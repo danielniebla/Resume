837b93f7fecdbfbab4c49dd12c872f849f89fd07
danieniebla <82241926+danielniebla@users.noreply.github.com>
2024-10-08 05:41:03 -0700

    despliegue

--- resume/src/components/experienceText.tsx
+++ resume/src/components/experienceText.tsx
@@ -17,7 +17,7 @@ const Exp1: React.FC<Exp> = ({title, textOne, textTwo, link}) => {
         <h2 className=" font-semibold ">{title}</h2>
         <p className="bb py-4">{textOne}</p>
         <div >
-        {open ? (<div><p className="py-4">{textTwo}</p> <a href={link} target="_blank"><p className='lmb py-2'>I wana know more</p></a><p className='lmb py-4' onClick={handleClick}>Less..</p></div>)
+        {open ? (<div><p className="py-4">{textTwo}</p> <a href={link} target="_blank" rel="noopener noreferrer"><p className='lmb py-2'>I wana know more</p></a><p className='lmb py-4' onClick={handleClick}>Less..</p></div>)
         : (<p className="py-4 lmb" onClick={handleClick}>More...</p>)}
         </div>
 
