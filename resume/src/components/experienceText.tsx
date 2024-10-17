5cdd6d0da97c63d99b2141ed1dbb7a0f3a75ab5c
danieniebla <82241926+danielniebla@users.noreply.github.com>
2024-10-16 21:38:06 -0700

    justify

--- resume/src/components/experienceText.tsx
+++ resume/src/components/experienceText.tsx
@@ -19,9 +19,9 @@ const Exp1: React.FC<Exp> = ({title,titulo, textOne, textoUno, textTwo,textoDos,
   return (
       <div className="box-1 rounded-2xl p-4 ">
         <h2 className=" font-semibold ">{language?title:titulo}</h2>
-        <p className="bb py-4">{language?textOne:textoUno}</p>
+        <p className="bb py-4 text-justify">{language?textOne:textoUno}</p>
         <div >
-        {open ? (<div><p className="py-4">{language?textTwo:textoDos}</p> <a href={link} target="_blank" rel="noopener noreferrer"><p className='lmb py-2'>I wana know more</p></a><p className='lmb py-4' onClick={handleClick}>Less..</p></div>)
+        {open ? (<div><p className="py-4 text-justify">{language?textTwo:textoDos}</p> <a href={link} target="_blank" rel="noopener noreferrer"><p className='lmb py-2'>I wana know more</p></a><p className='lmb py-4' onClick={handleClick}>Less..</p></div>)
         : (<p className="py-4 lmb" onClick={handleClick}>More...</p>)}
         </div>
 
